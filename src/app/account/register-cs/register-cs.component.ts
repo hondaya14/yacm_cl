0c98f5e36ee23ec16e99ce7f322cc56176ec7082
Yasuhisa Honda <nqvno14@gmail.com>
2022-10-14 18:07:40 +0900

    Implement form item validation & simple error message

--- src/app/account/register-cs/register-cs.component.ts
+++ src/app/account/register-cs/register-cs.component.ts
@@ -9,6 +9,7 @@ import {HttpClient} from "@angular/common/http";
 })
 export class RegisterCsComponent implements OnInit {
   ldap: LdapInfo = new LdapInfo();
+  infoInvalid: boolean = false;
   hide: boolean = true;
 
   constructor(private http: HttpClient) {
@@ -23,6 +24,8 @@ export class RegisterCsComponent implements OnInit {
   }
 
   register() {
+    this.infoInvalid = !this.ldap.checkInfo();
+    if (this.infoInvalid) return
     let endpoint: string = 'register_cs'
     let url: string = 'http://localhost:4000' + endpoint;
     this.http.post(url, JSON.stringify(this.ldap));
@@ -36,12 +39,27 @@ class LdapInfo {
   password: string;
 
   constructor() {
-    this.lastName = ''
-    this.firstName = ''
-    this.accountName = ''
-    this.password = ''
+    this.lastName = '';
+    this.firstName = '';
+    this.accountName = '';
+    this.password = '';
   }
 
+  checkInfo() {
+    return this.checkName(this.lastName)
+      && this.checkName(this.firstName)
+      && this.checkName(this.accountName)
+      && this.lastName.length > 0 && this.firstName.length > 0 && this.accountName.length > 0
+      && this.checkPassword();
+  }
 
+  checkName(checkStr: string) {
+    const re = new RegExp(/^[0-9a-zA-Z]+$/);
+    return re.test(checkStr);
+  }
+
+  checkPassword() {
+    const re = new RegExp(/^(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{8,}$/);
+    return re.test(this.password);
+  }
 }
-
--- src/app/account/register-mail/register-mail.component.ts
+++ src/app/account/register-mail/register-mail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-register-mail',
@@ -6,10 +7,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-mail.component.css']
 })
 export class RegisterMailComponent implements OnInit {
+  hide: boolean = true;
+  mail: MailAccountInfo = new MailAccountInfo();
+  infoInvalid: boolean = false;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit(): void {
   }
+
+  debug() {
+    console.log();
+  }
+
+  register() {
+    this.infoInvalid = !this.mail.checkInfo();
+    if (this.infoInvalid) return
+    let endpoint: string = 'register_cs'
+    let url: string = 'http://localhost:4000' + endpoint;
+    this.http.post(url, JSON.stringify(this.mail));
+  }
+}
+
+class MailAccountInfo {
+  userName: string;
+  password: string;
+  passwordInvalid: boolean;
+
+  constructor() {
+    this.userName = '';
+    this.password = '';
+    this.passwordInvalid = false;
+  }
+
+
+  checkInfo() {
+    return this.checkName(this.userName) && this.userName.length > 0 && this.checkPassword();
+  }
+
+  checkName(checkStr: string) {
+    const re = new RegExp(/^[0-9a-zA-Z]+$/);
+    return re.test(checkStr);
+  }
+
+  checkPassword() {
+    const re = new RegExp(/^(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{8,}$/);
+    return re.test(this.password);
+  }
+
 
 }
